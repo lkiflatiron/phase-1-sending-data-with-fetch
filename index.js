9f03674e562a85a31bcd742080df21980dfed752
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-30 17:48:19 -0500

    commit tree 68026bf4b386572e7770eac0ccfdbc7dc1948671

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+// const formData = {
+//   dogName: "Otis",
+//   dogBreed: "Labrador Retriever",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+
+
+
+
+function submitData(userName, userEmail) {
+  const userData = {
+    name: userName,
+    email: userEmail
+  }
+
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+
+  function writeMessage(message) {
+    const body = document.querySelector('body')
+    const p = document.createElement('p')
+    p.textContent = message
+    body.appendChild(p)
+  }
+
+  return fetch("http://localhost:3000/users", configurationObject)
+          .then(res => res.json())
+          .then(dataObj => {
+            writeMessage(dataObj.id)
+          })
+          .catch(e => {
+            writeMessage(e.message)
+          })
+}
+
+submitData("louise", "louise@gmail.com")
